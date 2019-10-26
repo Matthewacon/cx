af34d738f80d7454ac6bf568208f85d46ea747ab
Matthewacon <matthewjbarichello@gmail.com>
2019-10-26 18:43:05 -0400

    Removed union_cast class template

--- cx/unsafe.h
+++ cx/unsafe.h
@@ -1,26 +1,13 @@
 #pragma once
 
 namespace CX {
- namespace Internal {
-  template<typename T1, typename T2>
-  union union_cast_t {
-  private:
-   T1 t1;
-   T2 t2;
-
-  public:
-   constexpr union_cast_t(T2 t2) noexcept :
-    t2(t2)
-   {}
-
-   constexpr T1& operator()() noexcept {
-    return t1;
-   }
+ template<typename R, typename T>
+ constexpr R union_cast(T t) noexcept {
+  union cast {
+   T t;
+   R r;
+   constexpr cast(T t) noexcept : t(t) {}
   };
- }
-
- template<typename T1, typename T2>
- constexpr T1& union_cast(T2 t2) noexcept {
-  return Internal::union_cast_t<T1, T2>(t2)();
+  return cast{t}.r;
  }
 }

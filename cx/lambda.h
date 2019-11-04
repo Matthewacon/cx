93ee3cf58d2e101dc70f080bda8a1a78bce8753f
Matthewacon <matthewjbarichello@gmail.com>
2019-11-04 16:37:34 -0500

    Multiple changes:  - Added a CX::Lambda<T> type that is functionally identical to std::function but half the size  - Added a packed utility struct for composing and decomposing member function pointers

---
+++ cx/lambda.h
@@ -0,0 +1,136 @@
+#pragma once
+
+#include "cx/common.h"
+#include "cx/idioms.h"
+#include "cx/unsafe.h"
+
+namespace CX {
+ namespace Internal {
+  template<typename T>
+  struct FunctionCapture;
+
+  //non-capturing lambdas are implicitly convertible to plain function pointers
+  template<typename R, typename... Args>
+  struct FunctionCapture<R (Args...)> {
+   using functor_t = FunctionCapture<R (Args...)>;
+
+   constexpr FunctionCapture() noexcept :
+    type(UINIT),
+    functor(nullptr)
+   {}
+
+   constexpr FunctionCapture(R (*func)(Args...)) noexcept :
+    type(PLAIN),
+    functor(union_cast<char *>(func))
+   {}
+
+   template<typename T>
+   constexpr FunctionCapture(T inst) noexcept :
+    type(ANONYMOUS),
+    functor([=]() {
+     auto data = new char[sizeof(T) + sizeof(void *)];
+     constexpr R (T::* const func)(Args...) const = &T::operator();
+     *((void **)data) = union_cast<member_ptr_align_t>(func).ptr;
+     memcpy(data + sizeof(void *), &inst, sizeof(T));
+     return data;
+    }())
+   {
+    static_assert(FunctionOperatorExists<T>::value);
+   }
+
+   ~FunctionCapture() {
+    cleanup();
+   }
+
+   //TODO anonymous struct size required...
+// functor_t& operator=(const FunctionCapture<R (Args...)>& other) const noexcept {
+//  cleanup();
+//  switch (other.type) {
+//   case PLAIN: {
+//    functor = other.functor;
+//    type = PLAIN;
+//    break;
+//   }
+//   case ANONYMOUS: {
+//    functor = ;
+//    type = ANONYMOUS;
+//    break;
+//   }
+//  }
+//  return *this;
+// }
+
+   functor_t& operator=(R (* const func)(Args...)) const noexcept {
+    cleanup();
+    functor = union_cast<char *>(func);
+    type = PLAIN;
+    return *this;
+   }
+
+   template<typename T>
+   functor_t& operator=(T inst) const noexcept {
+    cleanup();
+    functor = new char[sizeof(T) + sizeof(void *)];
+    type = ANONYMOUS;
+    constexpr R (T::* const func)(Args...) const = &T::operator();
+    *((void **)functor) = union_cast<member_ptr_align_t>(func).ptr;
+    memcpy(data + sizeof(void *), &inst, sizeof(T));
+    return *this;
+   }
+
+   [[gnu::always_inline]]
+   inline R operator()(Args... args) const {
+    switch(type) {
+     case PLAIN: return (union_cast<R (*)(Args...)>(functor))(args...);
+     case ANONYMOUS: {
+      auto memPtr = member_ptr_align_t{*((void **)functor), nullptr};
+      return (union_cast<Dummy<> *>(functor + sizeof(void *))->*union_cast<R (Dummy<>::*)(Args...)>(memPtr))(args...);
+     }
+     case UINIT: throw std::runtime_error("FATAL: Lambda functor is uninitialized!");
+    }
+   }
+
+  private:
+   enum Type {
+    PLAIN,
+    ANONYMOUS,
+    UINIT
+   };
+
+   Type type;
+   char * const functor;
+
+   [[gnu::always_inline]]
+   inline void cleanup() const noexcept {
+    if (type == ANONYMOUS) {
+     delete[] functor;
+    }
+   }
+  };
+ }
+
+ template<typename F, bool = CX::IsFunction<F>::value>
+ struct Lambda;
+
+ template<typename R, typename... Args>
+ struct Lambda<R (Args...), true> {
+  using func_t = R (Args...);
+
+  constexpr Lambda() noexcept = default;
+
+  template<typename T>
+  constexpr Lambda(T t) : capture(t)
+  {}
+
+  constexpr Lambda(func_t * const func) : capture(func)
+  {}
+
+  [[gnu::always_inline nodiscard]]
+  inline constexpr R operator()(Args... args) const {
+   return capture(args...);
+  }
+
+ private:
+  Internal::FunctionCapture<func_t> capture;
+ };
+}
--- cx/unsafe.h
+++ cx/unsafe.h
@@ -1,6 +1,10 @@
 #pragma once
 
 namespace CX {
+ struct member_ptr_align_t {
+  void *ptr, *offset;
+ } __attribute__((packed));
+
  template<typename R, typename T>
  constexpr R union_cast(T t) noexcept {
   union cast {
